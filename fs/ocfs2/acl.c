c21872cde7e3d6d8402e966685c547e5cbae93ca
Jan Kara <jack@suse.cz>
2016-10-25 08:44:26 -0500

    [ALREADY APPLIED] posix_acl: Clear SGID bit when setting file permissions Note: Do mot cherry-pick this. This patch has been applied in a CAF tag
    commit 073931017b49d9458aa351605b43a7e34598caef upstream.
    
    When file permissions are modified via chmod(2) and the user is not in
    the owning group or capable of CAP_FSETID, the setgid bit is cleared in
    inode_change_ok().  Setting a POSIX ACL via setxattr(2) sets the file
    permissions as well as the new ACL, but doesn't clear the setgid bit in
    a similar way; this allows to bypass the check in chmod(2).  Fix that.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    [wt: dropped hfsplus changes : no xattr in 3.10]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- fs/ocfs2/acl.c
+++ fs/ocfs2/acl.c
@@ -275,7 +275,6 @@ static int ocfs2_set_acl(handle_t *handle,
 		name_index = OCFS2_XATTR_INDEX_POSIX_ACL_ACCESS;
 		if (acl) {
 			umode_t mode;
-
 			ret = posix_acl_update_mode(inode, &mode, &acl);
 			if (ret)
 				return ret;
