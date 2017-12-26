28773dc9b40f02fd50e5a72832364f64dcf0719e
anupritaisno1 <www.anuprita804@gmail.com>
2017-12-26 11:12:28 +0100

    travis: fix "travis: use ccache" commit 9624ad985e5439c9ad8de31e1d63609a557b7fd0 added support for ccache but it was not used. fix the paths so that ccache is now used
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- travis.sh
+++ travis.sh
@@ -1,12 +1,12 @@
 git clone https://bitbucket.org/anupritaisno1/aarch64-linux-gnu -b linaro
-export CROSS_COMPILE=./aarch64-linux-gnu/bin/aarch64-linux-gnu- ;
+export CROSS_COMPILE=/usr/bin/ccache ./aarch64-linux-gnu/bin/aarch64-linux-gnu- ;
 export ARCH=arm64;
 export SUBARCH=arm64;
 export KBUILD_BUILD_USER="Suzumiya";
 export KBUILD_BUILD_HOST="The_literary_club";
 STRIP="aarch64-linux-gnu/bin/aarch64-linux-gnu-strip";
 export CCOMPILE=$CROSS_COMPILE;
-export CROSS_COMPILE=aarch64-linux-gnu- ;
+export CROSS_COMPILE=/usr/bin/ccache aarch64-linux-gnu- ;
 export PATH=$PATH:./aarch64-linux-gnu/bin/ ;
 make haruhi_defconfig;
 make -j$(nproc --all);
